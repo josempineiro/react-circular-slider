6ec4cd38953724786e60b77556d0b799fdb54d99
Jose Manuel Piñeiro Garcia <josempga@ext.inditex.com>
2022-02-22 19:20:16 +0000

    Add Signature component

---
+++ components/Signature/Signature.jsx
@@ -0,0 +1,10 @@
+import styles from './Signature.module.css'
+
+const Signature = () => {
+  return (
+    <div className={styles.wrapper}>
+      <span className={styles.text}>Leman.dev</span>
+    </div>
+  )
+}
+export default Signature
